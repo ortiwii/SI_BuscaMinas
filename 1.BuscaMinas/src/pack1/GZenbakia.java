d5ff89d23b76e12f2b8db81235051ad9c3831790
ortiwii <ergorka@gmail.com>
2020-03-15 21:17:13 +0100

    gelaxken hasieraketa

---
+++ 1.BuscaMinas/src/pack1/GZenbakia.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class GZenbakia extends Gelaxka {
+
+	private int zenb;
+	
+	public GZenbakia(int pZut, int pEr, int pZenb) {
+		super (pZut, pEr);
+		this.zenb = pZenb;
+		
+	}
+	
+	public void desestali() {
+		//	TODO
+	}
+}
